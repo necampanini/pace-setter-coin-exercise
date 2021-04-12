6750bd93cd746c16e8ab80c2422fe5da6db2bd26
Nicholas Campanini <necampanini@gmail.com>
2021-04-12 17:29:20 -0400

    initial commit

--- PacesetterCoin/PricePoint.cs
+++ PacesetterCoin/PricePoint.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PacesetterCoin
 {
@@ -7,5 +9,30 @@ namespace PacesetterCoin
         public DateTime DateTime { get; set; }
 
         public decimal Price { get; set; }
+
+        public List<PricePoint> SubsequentPricePoints { get; set; } = new List<PricePoint>();
+
+        public PricePoint TargetPricePoint
+        {
+            get
+            {
+                if (!SubsequentPricePoints.Any()) return null;
+
+                return SubsequentPricePoints
+                    .OrderByDescending(spp => spp.Price)
+                    .First();
+            }
+        }
+
+        public decimal DifferenceFromHighestSubsequentPrice
+        {
+            get
+            {
+                if (TargetPricePoint == null) return 0;
+                var highest = TargetPricePoint.Price;
+
+                return highest - this.Price;
+            }
+        }
     }
 }
--- PacesetterCoin/Program.cs
+++ PacesetterCoin/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace PacesetterCoin
 {
@@ -6,7 +9,64 @@ namespace PacesetterCoin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // read in file, make ordered list of price point objects (date & price)
+            var pricePoints = MakePricePoints();
+
+            foreach (var current in pricePoints)
+            {
+                // using LINQ on the price point object, get the highest price point in all subsequent dates
+                current.SubsequentPricePoints = pricePoints.Where(p => p.DateTime > current.DateTime).ToList();
+            }
+
+            var buyOn = pricePoints.OrderByDescending(p => p.DifferenceFromHighestSubsequentPrice).First();
+
+            Console.WriteLine($"buy on: {buyOn.DateTime} at {buyOn.Price}");
+            Console.WriteLine($"to sell on : {buyOn.TargetPricePoint.DateTime} for {buyOn.TargetPricePoint.Price}");
+
+            var profit = buyOn.TargetPricePoint.Price - buyOn.Price;
+
+            // not positive if there's some special return percentage calculation
+            // assuming its just dividing profit by investment, then multiplied by 100
+
+            var percentageReturn = (profit / buyOn.Price) * 100;
+            Console.WriteLine($"Profit {profit:C}");
+            Console.WriteLine($"for return of {percentageReturn:N}%");
+
+        }
+
+        private static List<PricePoint> MakePricePoints()
+        {
+            var list = new List<PricePoint>();
+
+            string line;
+            var file = new StreamReader(@"C:\2018-pacesetter-coin.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                var array = line.Split('\t');
+
+                // skip first line
+                if (array[0] == "Date")
+                {
+                    continue;
+                }
+
+                // could use try/catches for safety
+                DateTime.TryParse(array[0], out var date);
+                decimal.TryParse(array[1], out var price);
+
+                // can parse
+                if (date == DateTime.MinValue) continue;
+
+                var newPricePoint = new PricePoint
+                {
+                    DateTime = date, Price = price
+                };
+
+                list.Add(newPricePoint);
+            }
+
+            return list.OrderBy(x => x.DateTime).ToList();
         }
     }
 }
